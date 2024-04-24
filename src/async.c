76330d97dfb42fcb1d3468e7311d6471dd262734
Ethan Stoneman <etst9006@colorado.edu>
2024-04-24 13:49:30 -0600

    sync changes to `DFCOperation` struct

--- src/async.c
+++ src/async.c
@@ -27,6 +27,7 @@ void *handle_put(void *arg) {
   size_t len_file;
 
   pthread_rwlock_rdlock(&conf_lock);
+  fprintf(stderr, "[%s] acquired rdlock on 'conf lock'\n", __func__);
 
   // read input file
   if ((file_contents = read_file(dfc_op->filename, &len_file)) == NULL) {
@@ -35,7 +36,7 @@ void *handle_put(void *arg) {
     return NULL;
   }
 
-  chunk_sizes = malloc(sizeof(size_t) * dfc_op->dfc_config.n_servers);
+  chunk_sizes = malloc(sizeof(size_t) * dfc_op->dfc_config->n_servers);
   if (chk_alloc_err(chunk_sizes, "malloc", __func__, __LINE__ - 1) == -1) {
     fprintf(stderr, "[ERROR] out of memory\n");
     free(file_contents);
@@ -44,50 +45,39 @@ void *handle_put(void *arg) {
   }
 
   // split file
-  fprintf(stderr, "getting chunk sizes given:\n");
-  fprintf(stderr, "  path to file: %s\n", dfc_op->filename);
-  fprintf(stderr, "  length of file: %zu\n", len_file);
-  fprintf(stderr, "  no. servers: %zu\n", dfc_op->dfc_config.n_servers);
-
-  get_chunk_sizes(len_file, dfc_op->dfc_config.n_servers, chunk_sizes);
+  get_chunk_sizes(len_file, dfc_op->dfc_config->n_servers, chunk_sizes);
   if ((chunks = split_file(file_contents, chunk_sizes,
-                           dfc_op->dfc_config.n_servers)) == NULL) {
+                           dfc_op->dfc_config->n_servers)) == NULL) {
     fprintf(stderr, "[ERROR] unable to chunkify %s\n", dfc_op->filename);
     free(file_contents);
 
     exit(EXIT_FAILURE);
   }
 
-  for (size_t i = 0; i < dfc_op->dfc_config.n_servers; ++i) {
-    fprintf(stderr, "\n\n[INFO] server: %s\n", dfc_op->dfc_config.servers[i]);
-    fprintf(stderr, "       sending chunk %zu (size=%zu)\n\n", i,
-            chunk_sizes[i]);
-    fwrite(chunks[i], sizeof(char), chunk_sizes[i], stderr);
-  }
-
   // free resources
-  for (size_t i = 0; i < dfc_op->dfc_config.n_servers; ++i) {
+  for (size_t i = 0; i < dfc_op->dfc_config->n_servers; ++i) {
     free(chunks[i]);
   }
+
+  pthread_rwlock_unlock(&conf_lock);
+  fprintf(stderr, "[%s] released rdlock on 'conf lock'\n", __func__);
 
   free(chunks);
   free(chunk_sizes);
   free(file_contents);
-
-  pthread_rwlock_unlock(&conf_lock);
 
   return NULL;
 }
 
 void *read_config(void *arg) {
   DFCConfig *dfc_config = (DFCConfig *)arg;
-  (void)dfc_config;
 
   char line[CONF_MAXLINE + 1];
   FILE *fp;
   size_t n_cols, addr_offset, n_servers;
 
   pthread_rwlock_wrlock(&conf_lock);
+  fprintf(stderr, "[%s] acquired wrlock on 'conf_lock'\n", __func__);
   if ((fp = fopen(DFC_CONF, "r")) == NULL) {
     fprintf(stderr, "[ERROR] unable to open %s\n", DFC_CONF);
 
@@ -114,6 +104,7 @@ void *read_config(void *arg) {
 
   dfc_config->n_servers = n_servers;
   pthread_rwlock_unlock(&conf_lock);
+  fprintf(stderr, "[%s] released wrlock held by 'conf lock'\n", __func__);
 
   return NULL;
 }
