73399d2a2c8d4b5aa9eb3639717ccb954f20d646
Ethan Stoneman <etst9006@colorado.edu>
2024-04-30 09:11:04 -0600

    abstract running command handlers to new function

--- src/dfc.c
+++ src/dfc.c
@@ -12,11 +12,97 @@ DFCCommand dfc_cmds[] = {{.cmd = "get", .hash = 0},
                          {.cmd = "list", .hash = 0},
                          {.cmd = "put", .hash = 0}};
 
+int run_handler(char **argv, unsigned int cmd_hash, size_t n_files) {
+  DFCConfig *dfc_config;
+  DFCOperation dfc_op[n_files];
+  pthread_t file_proc_tids[n_files];
+
+  if ((dfc_config = read_config()) == NULL) {
+    fprintf(stderr, "[ERROR] failed to read config\n");
+
+    return EXIT_FAILURE;
+  }
+
+  if (cmd_hash == hash_fnv1a("get")) {
+    if (n_files == 0) {
+      fprintf(
+          stderr,
+          "[ERROR] invalid get invocation, requires at least one file path\n");
+      usage(argv[0]);
+
+      return EXIT_FAILURE;
+    }
+
+    for (size_t i = 0; i < n_files; ++i) {
+      strncpy(dfc_op[i].filename, argv[i], PATH_MAX);
+      dfc_op[i].dfc_config = dfc_config;
+
+
+      file_proc_tids[i] = i;
+      if (pthread_create(&file_proc_tids[i], NULL, handle_get, &dfc_op[i]) < 0) {
+        fprintf(stderr, "[ERROR] could not create thread: %s:%d\n", __func__,
+                __LINE__ - 1);
+        exit(EXIT_FAILURE);
+      }
+    }
+  } else if (cmd_hash == hash_fnv1a("put")) {
+    if (n_files == 0) {
+      fprintf(
+          stderr,
+          "[ERROR] invalid put invocation, requires at least one file path\n");
+      usage(argv[0]);
+
+      return EXIT_FAILURE;
+    }
+
+    for (size_t i = 0; i < n_files; ++i) {
+      strncpy(dfc_op[i].filename, argv[i], PATH_MAX);
+      dfc_op[i].dfc_config = dfc_config;
+
+      if (pthread_create(&file_proc_tids[i], NULL, handle_put, &dfc_op[i]) < 0) {
+        fprintf(stderr, "[ERROR] could not create thread: %s:%d\n", __func__,
+                __LINE__ - 1);
+        exit(EXIT_FAILURE);
+      }
+    }
+  } else {  // list
+    fprintf(stderr, "[INFO] querying for files... \n");
+
+    if (pthread_create(&file_proc_tids[0], NULL, handle_list, NULL) < 0) {
+      fprintf(stderr, "[ERROR] could not create thread: %s:%d\n", __func__,
+              __LINE__ - 2);
+      exit(EXIT_FAILURE);
+    }
+  }
+
+  // free resources
+  for (size_t i = 0; i < n_files; ++i) {
+    pthread_join(file_proc_tids[i], NULL);
+  }
+
+  for (size_t i = 0; i < dfc_config->n_servers; ++i) {
+    free(dfc_config->servers[i]);
+  }
+
+  free(dfc_config->servers);
+  free(dfc_config);
+
+  return 0;
+}
+
 void print_cmd(DFCCommand dfc_cmd) {
   fprintf(stderr, "DFCCommand = {\n");
   fprintf(stderr, "  cmd = %s\n", dfc_cmd.cmd);
   fprintf(stderr, "  hash = %d\n", dfc_cmd.hash);
   fprintf(stderr, "}\n");
+}
+
+void usage(const char *program) {
+  fprintf(stderr, "usage: %s <command> [filename] ... [filename]\n", program);
+  fprintf(stderr, "supported commands:\n");
+  for (size_t i = 0; i < N_CMD_SUPP; ++i) {
+    fprintf(stderr, "  - %s\n", dfc_cmds[i].cmd);
+  }
 }
 
 void print_cmds(void) {
@@ -40,21 +126,11 @@ void print_config(DFCConfig *dfc_config) {
   fputs("  }\n", stderr);
 }
 
-void usage(const char *program) {
-  fprintf(stderr, "usage: %s <command> [filename] ... [filename]\n", program);
-  fprintf(stderr, "supported commands:\n");
-  for (size_t i = 0; i < N_CMD_SUPP; ++i) {
-    fprintf(stderr, "  - %s\n", dfc_cmds[i].cmd);
-  }
-}
-
 int main(int argc, char *argv[]) {
   BloomFilter *bf;
   char cmd[SZ_ARG_MAX + 1];
   unsigned short cmd_hash;
-  pthread_t file_proc_tids[argc];
   size_t n_files;
-  DFCConfig *dfc_config;
 
   if (argc < 2) {
     fprintf(stderr, "[ERROR] not enough arguments supplied\n");
@@ -85,80 +161,21 @@ int main(int argc, char *argv[]) {
     return EXIT_FAILURE;
   }
 
-  if ((dfc_config = read_config()) == NULL) {
-    fprintf(stderr, "[ERROR] failed to read config\n");
-    exit(EXIT_FAILURE);
-  }
-
   // act on command (selected `hash_fnv1a` arbitrarily)
   cmd_hash = hash_fnv1a(cmd);
   n_files = argc - 2;
 
-  if (cmd_hash == hash_fnv1a("get")) {
-    if (n_files == 0) {
-      fprintf(
-          stderr,
-          "[ERROR] invalid get invocation, requires at least one file path\n");
-      usage(argv[0]);
+  argv += 2;  // shift cli args to access supplied filenames
 
-      return EXIT_FAILURE;
-    }
+  // given n filenames, run command on each of them
+  if (run_handler(argv, cmd_hash, n_files) != 0) {
+    fprintf(stderr, "[ERROR] %s handler failed\n", cmd);
+    destroy_bloom_filter(bf);
 
-    DFCOperation dfc_op[n_files];
-    for (size_t i = 2; i < (size_t)argc; ++i) {
-      strncpy(dfc_op[i - 2].filename, argv[i], PATH_MAX);
-      dfc_op[i - 2].dfc_config = dfc_config;
-
-
-      file_proc_tids[i] = i;
-      if (pthread_create(&file_proc_tids[i], NULL, handle_get, &dfc_op[i - 2]) < 0) {
-        fprintf(stderr, "[ERROR] could not create thread: %s:%d\n", __func__,
-                __LINE__ - 1);
-        exit(EXIT_FAILURE);
-      }
-    }
-  } else if (cmd_hash == hash_fnv1a("put")) {
-    if (n_files == 0) {
-      fprintf(
-          stderr,
-          "[ERROR] invalid put invocation, requires at least one file path\n");
-      usage(argv[0]);
-
-      return EXIT_FAILURE;
-    }
-
-    DFCOperation dfc_op[n_files];
-    for (size_t i = 2; i < (size_t)argc; ++i) {
-      strncpy(dfc_op[i - 2].filename, argv[i], PATH_MAX);
-      dfc_op[i - 2].dfc_config = dfc_config;
-
-      if (pthread_create(&file_proc_tids[i], NULL, handle_put, &dfc_op[i - 2]) < 0) {
-        fprintf(stderr, "[ERROR] could not create thread: %s:%d\n", __func__,
-                __LINE__ - 1);
-        exit(EXIT_FAILURE);
-      }
-    }
-  } else {  // list
-    fprintf(stderr, "[INFO] querying for files... \n");
-
-    if (pthread_create(&file_proc_tids[0], NULL, handle_put, NULL) < 0) {
-      fprintf(stderr, "[ERROR] could not create thread: %s:%d\n", __func__,
-              __LINE__ - 2);
-      exit(EXIT_FAILURE);
-    }
-  }
-
-  // free resources
-  for (size_t i = 2; i < (size_t)argc; ++i) {
-    pthread_join(file_proc_tids[i], NULL);
+    exit(EXIT_FAILURE);
   }
 
   destroy_bloom_filter(bf);
-  for (size_t i = 0; i < dfc_config->n_servers; ++i) {
-    free(dfc_config->servers[i]);
-  }
-  free(dfc_config->servers);
-  free(dfc_config);
 
   return EXIT_SUCCESS;
 }
